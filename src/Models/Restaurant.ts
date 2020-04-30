a7cb429e5cbf90d6e0ec5e558f936cdf275c29e6
dillonfromdallas <thedillonreyna@gmail.com>
2020-04-29 21:39:47 -0500

    Export interface for use, and set 'hours' key to require an Hours sshape

--- src/Models/Restaurant.ts
+++ src/Models/Restaurant.ts
@@ -1,15 +1,17 @@
-interface Restaurant {
+import { Hours } from "./Hours";
+
+export interface Restaurant {
   address1: string;
   attire: string;
   city: string;
-  genre: [string];
-  hours: [string];
+  genre: string[];
+  hours: Hours;
   id: string;
   lat: string;
   long: string;
   name: string;
   state: string;
-  tags: [string];
+  tags: string[];
   telephone: string;
   website: string;
   zip: string;
