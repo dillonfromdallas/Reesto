9bb6703fad3e906770f8e22095146c82876e4fc0
dillonfromdallas <thedillonreyna@gmail.com>
2020-04-30 04:51:59 -0500

    Update redux to fetch and set restaurant info properly

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,20 +1,32 @@
 import React, { useEffect } from "react";
-import styled from "styled-components";
-import { useDispatch } from "react-redux";
-import Container from "./Container";
+import { useDispatch, useSelector, RootStateOrAny } from "react-redux";
+import Container from "./AppContainer";
 import Header from "./Header";
-import { restaurantActions } from "../Redux/actions";
+import DataViewer from "./DataViewer";
 
 function App() {
+  const base = "https://code-challenge.spectrumtoolbox.com/api/restaurants";
+  const key = "q3MNxtfep8Gt";
   const dispatch = useDispatch();
 
+  const getData = () => {
+    return (dispatch: any) => {
+      fetch(base, { headers: { Authorization: `Api-Key ${key}` } })
+        .then(data => data.json())
+        .then(data => dispatch({ type: "GET", payload: data }));
+    };
+  };
+
   useEffect(() => {
-    dispatch({ type: restaurantActions.get });
+    dispatch(getData());
   }, []);
+
+  const { list } = useSelector((state: RootStateOrAny) => state.restaurants);
 
   return (
     <Container>
       <Header>Reesto</Header>
+      {list.length ? <DataViewer list={list} /> : <></>}
     </Container>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 import rootReducer from "./Redux/rootReducer";
 import "./index.css";
-import App from "./App";
+import App from "./Components/App";
 import * as serviceWorker from "./serviceWorker";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
