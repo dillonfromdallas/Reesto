3dedd2dc87ecf45bdb305ea971002c9d5ac0ccf9
dillonfromdallas <thedillonreyna@gmail.com>
2020-04-30 00:32:46 -0500

    Allow for 'color' and 'fontSize' props

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 import styled from "styled-components";
 
-const Header = styled.h1({
-  fontSize: 56
-});
 const Div = styled.div({
   flex: 1,
   flexDirection: "row",
   textAlign: "center",
   width: "100%"
 });
+
 interface Props {
   children: React.ReactNode;
+  fontSize?: number;
+  color?: string;
 }
 
 export default function(props: Props) {
+  const Header = styled.h1({
+    fontSize: props.fontSize ? props.fontSize : 32,
+    color: props.color ? props.color : "black"
+  });
   return (
     <Div>
       <Header>{props.children}</Header>
