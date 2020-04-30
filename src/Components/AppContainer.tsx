0697086a52ffadefd1c2a32010021d725f8cc8c9
dillonfromdallas <thedillonreyna@gmail.com>
2020-04-30 06:44:03 -0500

    Replace absolute positioning with Flex

--- src/Components/AppContainer.tsx
+++ src/Components/AppContainer.tsx
@@ -2,14 +2,10 @@ import React from "react";
 import styled from "styled-components";
 
 export default styled.div({
+  display: "flex",
   flex: 1,
   flexDirection: "column",
   backgroundColor: "#a7dd71",
-  position: "absolute",
-  top: 0,
-  left: 0,
-  right: 0,
-  bottom: 0,
   width: "100%",
   height: "100%",
   justifyContent: "center",
