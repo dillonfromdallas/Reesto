be34664368018e71c24c2c8e30a9f4324799670f
dillonfromdallas <thedillonreyna@gmail.com>
2020-04-30 06:44:26 -0500

    Extract redux actions to separate file

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -3,30 +3,27 @@ import { useDispatch, useSelector, RootStateOrAny } from "react-redux";
 import Container from "./AppContainer";
 import Header from "./Header";
 import DataViewer from "./DataViewer";
+import { getData, setCurrent } from "../Redux/actions";
 
 function App() {
-  const base = "https://code-challenge.spectrumtoolbox.com/api/restaurants";
-  const key = "q3MNxtfep8Gt";
   const dispatch = useDispatch();
-
-  const getData = () => {
-    return (dispatch: any) => {
-      fetch(base, { headers: { Authorization: `Api-Key ${key}` } })
-        .then(data => data.json())
-        .then(data => dispatch({ type: "GET", payload: data }));
-    };
-  };
 
   useEffect(() => {
     dispatch(getData());
   }, []);
 
-  const { list } = useSelector((state: RootStateOrAny) => state.restaurants);
+  const { list, current } = useSelector(
+    (state: RootStateOrAny) => state.restaurants
+  );
 
   return (
     <Container>
-      <Header>Reesto</Header>
-      {list.length ? <DataViewer list={list} /> : <></>}
+      <Header>Reesta</Header>
+      {current && current.length ? (
+        <DataViewer list={current} />
+      ) : (
+        <DataViewer list={list} />
+      )}
     </Container>
   );
 }
