a0d68d7cd4a701c13de3700a74e8550d50d06ece
dillonfromdallas <thedillonreyna@gmail.com>
2020-05-01 18:55:53 -0500

    Modify background color

--- src/Components/AppContainer.tsx
+++ src/Components/AppContainer.tsx
@@ -5,7 +5,7 @@ export default styled.div({
   display: "flex",
   flex: 1,
   flexDirection: "column",
-  backgroundColor: "#a7dd71",
+  backgroundColor: "#a8dbf7",
   width: "100%",
   height: "100%",
   justifyContent: "center",
